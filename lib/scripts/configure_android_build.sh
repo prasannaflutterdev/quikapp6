2660e7bd4a75ae546ee8986256a93b18867499b0
QuikAppTech <prasannasrie@gmail.com>
2025-06-10 17:27:18 +0530

    Android Build 08

--- lib/scripts/configure_android_build.sh
+++ lib/scripts/configure_android_build.sh
@@ -44,6 +44,12 @@ echo "🧩 Writing root Gradle files..."
 # EOF
 
 cat <<'EOF' > android/build.gradle.kts
+plugins {
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false # Added this line
+}
+
 allprojects {
     repositories {
         google()
