d20fa06c2bb17bd27a21dd15987f073bc324f169
QuikAppTech <prasannasrie@gmail.com>
2025-06-10 17:39:11 +0530

    Android Build 09

--- lib/scripts/configure_android_build.sh
+++ lib/scripts/configure_android_build.sh
@@ -47,7 +47,7 @@ cat <<'EOF' > android/build.gradle.kts
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false # Added this line
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 allprojects {
